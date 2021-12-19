09b37772e964d2878dca5bc248ba429654edf6db
Bob F <bobfdev@gmail.com>
2021-12-18 23:01:10 -0600

    updated files

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// mobile navbar menu
+
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
 
@@ -5,3 +7,25 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+// modal
+
+const modal = document.getElementById('email-modal');
+const openBtn = document.querySelector('.main-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+// click events
+
+openBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
+// clicking the close button to exit
+closeBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+// clicking outside the modal to exit
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
